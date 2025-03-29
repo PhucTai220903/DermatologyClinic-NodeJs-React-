abb8d432f19864204b7276d7668e90230f5a1a15
NguyenQuys <nguyenhuuquy110203@gmail.com>
2025-03-29 07:33:45 +0700

    update cart

--- BE/src/models/order.model.js
+++ BE/src/models/order.model.js
@@ -1,23 +1,35 @@
 const mongoose = require("mongoose");
 
-const orderSchema = new mongoose.Schema({
-    customer_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+const orderSchema = new mongoose.Schema(
+  {
+    customer_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
     items: {
-        type: [
-            {
-                item_id: { type: mongoose.Schema.Types.ObjectId, required: true },
-                item_name: { type: String, required: true },
-                type: { type: String, enum: ["medicine", "comestic", "treatment"], required: true },
-                quantity: { type: Number },
-                price: { type: Number, required: true }
-            }
-        ],
-        required: true
+      type: [
+        {
+          item_id: { type: mongoose.Schema.Types.ObjectId, required: true },
+          item_name: { type: String, required: true },
+          type: {
+            type: String,
+            enum: ["medicine", "comestic", "treatment"],
+            required: true,
+          },
+          quantity: { type: Number },
+          price: { type: Number, required: true },
+        },
+      ],
+      required: true,
     },
     total_amount: { type: Number, required: true },
     discount_applied: { type: Number, required: true },
     final_amount: { type: Number, required: true },
-}, { timestamps: true });
+    address: { type: String },
+  },
+  { timestamps: true }
+);
 
 const Order = mongoose.model("Order", orderSchema);
 module.exports = Order;
--- BE/src/services/cart.service.js
+++ BE/src/services/cart.service.js
@@ -1,88 +1,117 @@
 const _repository = require("../repositories/sub.repository");
 
 class CartService {
-    async getByCustomerId(currentUserId) {
-        let existingCart = await _repository.cartRepository.getCartByCustomerId(currentUserId);
+  async getByCustomerId(currentUserId) {
+    let existingCart = await _repository.cartRepository.getCartByCustomerId(
+      currentUserId
+    );
 
-        if (!existingCart) {
-            throw Object.assign(new Error("Không tìm thấy giỏ hàng của user này"), { status: 404 });
-        }
+    if (!existingCart) {
+      throw Object.assign(new Error("Không tìm thấy giỏ hàng của user này"), {
+        status: 404,
+      });
+    }
 
-        let updatedItems = await Promise.all(existingCart.items.map(async (item) => {
-            let cosmetic = await _repository.comesticRepository.getById(item.comestic_id);
-            return {
-                _id: item.id,
-                comestic_id: item.comestic_id,
-                quantity: item.quantity,
-                comestic_name: cosmetic ? cosmetic.name : "Unknown"
-            };
-        }));
-
+    let updatedItems = await Promise.all(
+      existingCart.items.map(async (item) => {
+        let cosmetic = await _repository.comesticRepository.getById(
+          item.comestic_id
+        );
         return {
-            _id: existingCart.id,
-            customer_id: existingCart.customer_id,
-            items: updatedItems,
+          _id: item.id,
+          comestic_id: item.comestic_id,
+          comestic_image: cosmetic.image,
+          price: cosmetic.price,
+          quantity: item.quantity,
+          comestic_name: cosmetic ? cosmetic.name : "Unknown",
         };
+      })
+    );
+
+    return {
+      _id: existingCart.id,
+      customer_id: existingCart.customer_id,
+      items: updatedItems,
+    };
+  }
+
+  async add(currentUserId, cartRequest) {
+    let existingCart = await _repository.cartRepository.getCartByCustomerId(
+      currentUserId
+    );
+
+    if (!existingCart) {
+      const newCart = {
+        customer_id: currentUserId,
+        items: [cartRequest],
+      };
+      await _repository.cartRepository.add(newCart);
+    } else {
+      let existingItem = existingCart.items.find((item) =>
+        item.comestic_id.equals(cartRequest.comestic_id)
+      );
+
+      if (existingItem) {
+        existingItem.quantity += cartRequest.quantity;
+      } else {
+        existingCart.items.push(cartRequest);
+      }
+
+      await _repository.cartRepository.update(existingCart.id, existingCart);
     }
 
-    async add(currentUserId, cartRequest) {
-        let existingCart = await _repository.cartRepository.getCartByCustomerId(currentUserId);
+    return "Thêm thành công";
+  }
 
-        if (!existingCart) {
-            const newCart = {
-                customer_id: currentUserId,
-                items: [cartRequest]
-            };
-            await _repository.cartRepository.add(newCart);
-        } else {
-            let existingItem = existingCart.items.find(item => item.comestic_id.equals(cartRequest.comestic_id));
-
-            if (existingItem) {
-                existingItem.quantity += order.quantity;
-            } else {
-                existingCart.items.push(cartRequest);
-            }
-
-            await _repository.cartRepository.update(existingCart.id, existingCart);
-        }
-
-        return "Thêm thành công";
+  async update(currentUserId, cartRequest) {
+    let existingCart = await _repository.cartRepository.getCartByCustomerId(
+      currentUserId
+    );
+    if (!existingCart) {
+      throw Object.assign(new Error("Không tìm thấy giỏ hàng của user này"), {
+        status: 404,
+      });
     }
 
-    async update(currentUserId, cartRequest) {
-        let existingCart = await _repository.cartRepository.getCartByCustomerId(currentUserId);
-        if (!existingCart) {
-            throw Object.assign(new Error("Không tìm thấy giỏ hàng của user này"), { status: 404 });
-        }
+    let updatedItem = existingCart.items.find((item) =>
+      item.comestic_id.equals(cartRequest.comestic_id)
+    );
 
-        let updatedItem = existingCart.items.find(item => item.comestic_id.equals(cartRequest.comestic_id));
-
-        if (updatedItem) {
-            updatedItem.quantity += cartRequest.quantity;
-            if (updatedItem.quantity == 0) {
-                existingCart.items.splice(updatedItem, 1);
-            }
-        }
-
-        await _repository.cartRepository.update(existingCart._id, existingCart);
-
-        return "Đã cập nhật thành công";
+    if (updatedItem) {
+      updatedItem.quantity += cartRequest.quantity;
+      if (updatedItem.quantity == 0) {
+        existingCart.items.splice(updatedItem, 1);
+      }
     }
-    
-    async delete(currentUserId, cartToDelete){
-        let existingCart = await _repository.cartRepository.getCartByCustomerId(currentUserId);
-        if (!existingCart) {
-            throw Object.assign(new Error("Không tìm thấy giỏ hàng của user này"), { status: 404 });
-        }
 
-        let deletedItem = existingCart.items.find(item => item.comestic_id.equals(cartToDelete.comestic_id));
+    await _repository.cartRepository.update(existingCart._id, existingCart);
 
-        if(!deletedItem)
-            throw Object.assign(new Error("Không tìm thấy sản phẩm này trong giỏ hàngg"), { status: 404 });
-        existingCart.items.splice(deletedItem,1);
-        await _repository.cartRepository.update(existingCart._id, existingCart);
-        return "Xóa sản phẩm khỏi giỏ hàng thành công";
+    return "Đã cập nhật thành công";
+  }
+
+  async delete(currentUserId, cartToDelete) {
+    let existingCart = await _repository.cartRepository.getCartByCustomerId(
+      currentUserId
+    );
+    if (!existingCart) {
+      throw Object.assign(new Error("Không tìm thấy giỏ hàng của user này"), {
+        status: 404,
+      });
     }
+
+    let deletedItem = existingCart.items.find((item) =>
+      item.comestic_id.equals(cartToDelete)
+    );
+
+    if (!deletedItem)
+      throw Object.assign(
+        new Error("Không tìm thấy sản phẩm này trong giỏ hàngg"),
+        { status: 404 }
+      );
+    existingCart.items.splice(deletedItem, 1);
+    await _repository.cartRepository.update(existingCart._id, existingCart);
+    return "Xóa sản phẩm khỏi giỏ hàng thành công";
+  }
 }
 
 module.exports = new CartService();
